75f4cfaa3ef01b3fbe392fe6aea822801b401238
Eduardo Montelongo <montelongodevelopment@gmail.com>
2024-04-15 23:44:30 +1000

    created arch config, optimised other aspects

---
+++ init_arch.sh
@@ -0,0 +1,46 @@
+#!bin/bash
+
+#install yay
+pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+#update AUR
+yay
+
+# Essentials
+pacman -S --noconfirm \
+	zsh \
+	git \
+	curl \
+	wget \
+	neovim
+
+# Terminal
+pacman -S --noconfirm \
+	fd \
+	thefuck \
+	ripgrep \
+  lazygit
+
+# Configure thefuck
+fuck
+fuck
+source ~/.bashrc
+
+# Make packages scripts executable
+chmod +x ./packages/*.sh
+
+# Other
+./packages/oh-my-zsh.sh
+./packages/kitty.sh
+./packages/fzf.sh
+./packages/rust.sh
+
+# Delete zshrc and thefuck/settings.py before creating symlink
+rm /home/eduardo/.zshrc
+rm /home/eduardo/.config/thefuck/settings.py
+
+# Run Dotter
+dotter deploy
--- packages/oh-my-zsh.sh
+++ packages/oh-my-zsh.sh
@@ -1,2 +1,3 @@
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+git clone https://github.com/zsh-users/zsh-autosuggestions ../zsh/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-completions ../zsh/plugins/zsh-completions

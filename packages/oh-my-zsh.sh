2e98666719023304221a93d88ad6ab430e0574fd
Eduardo Montelongo <145420564+OnePoncho@users.noreply.github.com>
2024-04-16 19:38:34 +1000

    Update oh-my-zsh.sh

--- packages/oh-my-zsh.sh
+++ packages/oh-my-zsh.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone https://github.com/zsh-users/zsh-autosuggestions ../zsh/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-completions ../zsh/plugins/zsh-completions

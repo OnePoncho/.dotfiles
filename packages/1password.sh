6f9106ea17df2ef8cbe71b14bbaed5e766577c76
Eduardo Montelongo <145420564+OnePoncho@users.noreply.github.com>
2024-04-16 19:31:32 +1000

    Update 1password.sh

--- packages/1password.sh
+++ packages/1password.sh
@@ -1,8 +1,8 @@
+#!/usr/bin/bash
 # Get the 1Password signing key
  curl -sS https://downloads.1password.com/linux/keys/1password.asc | gpg --import
 
- cd
- git clone https://aur.archlinux.org/1password.git
+ git clone https://aur.archlinux.org/1password.git ~/.1password
 
- cd 1password
+ cd ~/.1password/
  makepkg -si

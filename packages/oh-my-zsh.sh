5b2321948b0ee1551269efb0eeba69938db18b9b
Eduardo Montelongo <145420564+OnePoncho@users.noreply.github.com>
2024-04-16 20:39:09 +1000

    fixes

--- packages/oh-my-zsh.sh
+++ packages/oh-my-zsh.sh
@@ -1,5 +1,6 @@
  #!/bin/bash
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+mkdir ../zsh/plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions ../zsh/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-completions ../zsh/plugins/zsh-completions

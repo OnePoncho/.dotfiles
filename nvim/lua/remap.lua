d15368e2a212149d167dd2d666fa3c3b026988da
OnePoncho <montelongodevelopment@gmail.com>
2024-05-23 17:03:24 +1000

    more remaps

--- nvim/lua/remap.lua
+++ nvim/lua/remap.lua
@@ -1,5 +1,16 @@
 -- generate uuid
 vim.keymap.set("n", "<leader>u", "i\"<C-r>=system('uuidgen')[:-2]<CR>\",<Esc>")
+
+-- diagnostic
 vim.keymap.set('n', '<leader>dp', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
 vim.keymap.set('n', '<leader>dn', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>do', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+
+
+-- cursor stays in the centre of the screen during these commands
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set('n', '<C-d>', '<C-d>zz', { noremap = true })
+vim.keymap.set('n', '<C-u>', '<C-u>zz', { noremap = true })
+vim.keymap.set('n', 'n', 'nzz', { noremap = true })
+vim.keymap.set('n', '<C-i>', '<C-i>zz', { noremap = true })
+vim.keymap.set('n', '<C-o>', '<C-o>zz', { noremap = true })

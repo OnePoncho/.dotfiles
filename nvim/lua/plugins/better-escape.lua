8c7b02f673f8e987ba9293dc84694be4547a5814
Eduardo Montelongo <eduardo.montelongo@visualcortex.com>
2024-07-24 13:33:37 +1000

    'better escape patch'

--- nvim/lua/plugins/better-escape.lua
+++ nvim/lua/plugins/better-escape.lua
@@ -1,11 +1,25 @@
 return {
+
   {
     'max397574/better-escape.nvim',
     config = function()
+      local escape_mapping = {
+        j = {
+          k = "<Esc>",
+        },
+        k = {
+          j = "<Esc>",
+        },
+      }
       require 'better_escape'.setup {
-        mapping = { "jk", "kj" }
+        default_mappings = false,
+        mappings = {
+          i = escape_mapping,
+          c = escape_mapping,
+          t = escape_mapping,
+        },
       }
     end,
-
   },
 }
+

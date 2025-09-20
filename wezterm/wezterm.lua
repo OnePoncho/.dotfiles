5e80714e7a968257dfc18fe67b62fa5158df2f34
OnePoncho <montelongodevelopment@gmail.com>
2025-09-20 17:05:00 +1000

    Added ghostty config changed dotter globals

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,15 +4,13 @@ local wezterm = require 'wezterm'
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices.
+term = "xterm-256color"
 
 -- For example, changing the initial geometry for new windows:
 config.initial_cols = 120
 config.initial_rows = 28
-
--- or, changing the font size and color scheme.
-config.font_size = 14
 config.adjust_window_size_when_changing_font_size = false
+config.font_size = 14
 
 
 -- FIGURE OUT COLOR SCHEMES
@@ -26,7 +24,13 @@ config.adjust_window_size_when_changing_font_size = false
 
 config.window_background_opacity = 0.9
 
+config.show_tabs_in_tab_bar = true
 config.tab_bar_at_bottom = true
+config.tab_max_width = 20
+config.hide_tab_bar_if_only_one_tab = true
+config.show_new_tab_button_in_tab_bar = true
+config.show_tab_index_in_tab_bar = true
+config.tab_and_split_indices_are_zero_based = false
 
 config.keys = {
   {
@@ -41,7 +45,11 @@ config.keys = {
   key = '+',
   mods = 'CMD',
   action = wezterm.action.IncreaseFontSize
-},
+}, {
+  key = 'X',
+  mods = 'CTRL|SHIFT',
+  action = wezterm.action.ActivateCopyMode
+}
 
 }
 -- Finally, return the configuration to wezterm:

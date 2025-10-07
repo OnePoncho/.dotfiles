316252c2b896aa12138c5bc7533545d4649a0be1
OnePoncho <montelongodevelopment@gmail.com>
2025-10-07 21:54:25 +1100

    hammerspoon configs

--- dotter.sh
+++ dotter.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# syntax - file/dir that already exists -- where you want it to go
 ln -sf ~/dotfiles/zsh/plugins ~/.oh-my-zsh/custom
 ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc 
 ln -sf ~/dotfiles/ghostty ~/.config
 ln -sf ~/dotfiles/nvim ~/.config
-ln -sf ~/dotfiles/hammerspoon/init.lua ~/.hammerspoon/init.lua
+ln -sf ~/dotfiles/hammerspoon ~/.hammerspoon
+ln -sf ~/dotfiles/aerospace/aerospace.toml ~/.aerospace.toml
 
 # Fonts only for Arch
 #ln -sf ~/dotfiles/fonts ~/.local/share

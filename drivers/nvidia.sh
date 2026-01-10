f2eabe340ca55feb568965828f0b04a5a308624a
OnePoncho <montelongodevelopment@gmail.com>
2026-01-10 15:49:11 +1100

    clean up

--- drivers/nvidia.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-pacman -S --needed nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings vulkan-icd-loader lib32-vulkan-icd-loader
--- packages/1password.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Get the 1Password signing key
- curl -sS https://downloads.1password.com/linux/keys/1password.asc | gpg --import
-
- git clone https://aur.archlinux.org/1password.git ~/.1password
-
- cd ~/.1password/
- makepkg -si
